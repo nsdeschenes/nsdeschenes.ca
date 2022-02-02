14a44819b272e467b81fc56978f4a22fa3c1e475
nsdeschenes <7891920+nsdeschenes@users.noreply.github.com>
2022-02-02 19:00:09 -0400

    setup custom theme and get tailwindcss to finally be happy

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,15 @@
 module.exports = {
   content: ['./app/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'background': '#222831',
+        'background-light': '#393E46',
+        'hightlight': '#FFD369',
+        'highlight-text': '#9840B3',
+        'main-text': '#EEEEEE',
+      },
+    },
   },
   plugins: [],
 }
